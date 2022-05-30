8445ed17c641cdaf0c6c1be0457c9fb6a74f8f40
dev.farahani <dev.farahani@gmail.com>
2022-05-30 22:05:55 +0430

    update user model

--- src/Model/User.php
+++ src/Model/User.php
@@ -2,7 +2,21 @@
 
 namespace Vandar\Sso\Model;
 
-class User 
+class User
 {
-    
+    public $id;
+
+    public $mobile;
+
+    public $fName;
+
+    public $lName;
+
+    public function __construct(array $userDara)
+    {
+        $this->id = $userDara['id'];
+        $this->mobile = $userDara['mobile'];
+        $this->fName = $userDara['f_name'];
+        $this->lName = $userDara['l_name'];
+    }
 }
