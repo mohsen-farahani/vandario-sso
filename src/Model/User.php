a161cee65e25f6332230dce73545239fb729caf1
dev.farahani <dev.farahani@gmail.com>
2022-06-07 18:50:39 +0430

    update model user

--- src/Model/User.php
+++ src/Model/User.php
@@ -8,15 +8,30 @@ class User
 
     public $mobile;
 
-    public $fName;
+    public $email;
 
-    public $lName;
+    public $fname;
+
+    public $lname;
+
+    public $fullName;
+
+    public $nationalCode;
+
+    public $birthDate;
+
+    public $auth;
 
     public function __construct(array $userDara)
     {
         $this->id = $userDara['id'];
         $this->mobile = $userDara['mobile'];
-        $this->fName = $userDara['f_name'];
-        $this->lName = $userDara['l_name'];
+        $this->email = $userDara['email'];
+        $this->fname = $userDara['fname'];
+        $this->lname = $userDara['lname'];
+        $this->fullName = $userDara['full_name'];
+        $this->nationalCode = $userDara['national_code'];
+        $this->birthDate = $userDara['birth_date'];
+        $this->auth = $userDara['auth'];
     }
 }
